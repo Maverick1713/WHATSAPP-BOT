0d5bd97dd87dbe6147ce7ebdb1d17b144570e5f9
Maverick1713 <140364364+Maverick1713@users.noreply.github.com>
2024-09-05 09:15:39 +0530

    Update bard_api.py

--- helper/bard_api.py
+++ helper/bard_api.py
@@ -5,7 +5,7 @@ from bardapi import Bard
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ['_BARD_API_KEY']='dQgwF2BbsvqHbMT2z1wQugaj0VDnZqJSKmt5c0madTuYq2RDanCx5MLhzXT7ZMEuy98ffg.'
+os.environ['_BARD_API_KEY']=''
 
 def text_generate(prompt:str):
     x= Bard().get_answer(prompt)['content']
